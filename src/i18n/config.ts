5b7344b50e084e94c4f493f216ae5f4a04a46dc5
Cal <151945511+cdm227@users.noreply.github.com>
2026-04-23 07:39:53 +0000

    i18n: add i18next configuration

---
+++ src/i18n/config.ts
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import enTranslations from './locales/en.json';
+import itTranslations from './locales/it.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: { translation: enTranslations },
+      it: { translation: itTranslations },
+    },
+  });
+
+export default i18n;
