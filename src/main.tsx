6ee9fd98fb704cdee60fb9db6e97e826cbe07de1
Cal <151945511+cdm227@users.noreply.github.com>
2026-04-23 07:39:59 +0000

    i18n: initialize i18next in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./index.css";
+import { Toaster } from "sonner";
+import "./i18n/config.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <Toaster position="top-center" />
   </React.StrictMode>
 );
